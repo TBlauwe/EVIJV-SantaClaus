5dd40576b055678ebfbae547eb04db4b374041b8
Tristan <tdb.work@outlook.fr>
2018-12-12 20:00:57 +0100

    Implemented basic game functionalities (playable)

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DropZone : MonoBehaviour {
 
-    private int counter;
+    public int counter;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +13,6 @@ public class DropZone : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(counter);
 	}
 
     private void OnTriggerEnter(Collider other)
