489b0253b215937f0a50e0b275999e5eae1979b9
Tristan <tdb.work@outlook.fr>
2019-01-15 16:36:15 +0100

    Added destroyable object

--- Assets/Scripts/Interaction/OnCollisionNoise.cs
+++ Assets/Scripts/Interaction/OnCollisionNoise.cs
@@ -9,6 +9,8 @@ public class OnCollisionNoise : MonoBehaviour {
     public float maxImpact = 10;
     public float volumeMin = 0;
     public float volumeMax = 1;
+    public bool destroy = false;
+    public GameObject onDestroyPrefab;
 
     private AudioSource audioSource;   // Sound played when moving a door
     private Rigidbody rigidBody;
@@ -29,6 +31,14 @@ public class OnCollisionNoise : MonoBehaviour {
             float volume = Mathf.Lerp(volumeMin, volumeMax, (value - min) / (maxImpact - min));
             audioSource.volume = volume;
             audioSource.Play();
+
+            if (volume == 1 && destroy)
+            {
+                var instance = Instantiate(onDestroyPrefab, gameObject.GetComponent<Collider>().bounds.center, transform.rotation);
+                //var main = instance.GetComponent<ParticleSystem>().main;
+                //main.duration = audioSource.clip.length;
+                Destroy(gameObject);
+            }
         }
     }
 }
